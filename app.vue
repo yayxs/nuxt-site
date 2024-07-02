82fc4f190d7a698189aa51c8ea0fc4ab42adc14f
yayxs <leaderywl@163.com>
2024-07-02 18:29:04 +0800

    feat: add md doc

--- app.vue
+++ app.vue
@@ -1,3 +1,3 @@
 <template>
-  App vue
+  <NuxtPage />
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/eslint"],
+  modules: ['@nuxt/eslint', '@nuxt/content'],
   // 开启 devtools 模式
-  devtools: { enabled: true },
+  devtools: { enabled: true }
 })
