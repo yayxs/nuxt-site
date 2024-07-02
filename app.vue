71cc87f3de7b7c412e93f378beb5c2c2890ccf5f
yayxs <leaderywl@163.com>
2024-07-02 18:02:43 +0800

    chore: init nuxt site add eslint

---
+++ app.vue
@@ -0,0 +1,3 @@
+<template>
+  App vue
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ["@nuxt/eslint"],
+  // 开启 devtools 模式
+  devtools: { enabled: true },
+})
